5c2a3ab0438aa97023d667de7b9334c201b7f4b2
Artem Lajko <artem.lajko@hpa.hamburg.de>
2023-09-21 10:43:10 +0200

    init push

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "random_id" "server_name" {
+  count = var.name == null ? 1 : 0
+
+  byte_length = 3
+}
+
+resource "random_password" "administrator_password" {
+  length  = 24
+  special = false
+  keepers = {
+    administrator_login = var.administrator_login
+  }
+}
+
+
+resource "azurerm_postgresql_flexible_server" "main" {
+  name                = var.name == null ? random_id.server_name[0].hex : var.name
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  administrator_login    = var.administrator_login
+  administrator_password = random_password.administrator_password.result
+
+  sku_name = var.sku_name
+  version  = var.engine_version
+
+  auto_grow_enabled            = var.storage["auto_grow"]
+  storage_mb                   = var.storage["size"]
+  backup_retention_days        = var.storage["backup_retention_days"]
+  geo_redundant_backup_enabled = var.storage["geo_redundant_backup"]
+  tags                         = var.tags
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "main" {
+  count            = length(var.trusted_network_cidr)
+  name             = element(keys(var.trusted_network_cidr), count.index)
+  server_id        = azurerm_postgresql_flexible_server.main.id
+  start_ip_address = cidrhost(element(values(var.trusted_network_cidr), count.index), 0)
+  end_ip_address   = cidrhost(element(values(var.trusted_network_cidr), count.index), -1)
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "name" {
+  type        = string
+  default     = null
+  description = "Name of the MariaDB server. If this input is not specified, a random ID is set as the name."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name in which the resources should be created"
+}
+
+# SKU
+variable "sku_name" {
+  default     = "GP_Standard_D4s_v3"
+  description = <<-EOT
+  The SKU Name for the PostgreSQL Flexible Server.
+  The name of the SKU, follows the tier + name pattern (e.g. B_Standard_B1ms, GP_Standard_D2s_v3, MO_Standard_E4s_v3
+  EOT
+}
+
+
+
+variable "storage" {
+  type = object({
+    auto_grow             = bool
+    size                  = number
+    backup_retention_days = number
+    geo_redundant_backup  = bool
+  })
+
+  default = {
+    auto_grow             = true
+    size                  = 32768
+    backup_retention_days = 30
+    geo_redundant_backup  = false
+  }
+
+  description = <<-EOT
+  auto_grow: Is the storage auto grow for PostgreSQL Flexible Server enabled? Defaults to false.
+  size: The max storage allowed for the PostgreSQL Flexible Server. Possible values are 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216 and 33553408
+  backup_retention_days: The backup retention days for the PostgreSQL Flexible Server. Possible values are between 7 and 35 days.
+  geo_redundant_backup: Is Geo-Redundant backup enabled on the PostgreSQL Flexible Server. Defaults to false. Changing this forces a new PostgreSQL Flexible Server to be created.
+  EOT
+}
+
+variable "administrator_login" {
+  type    = string
+  default = "pgsqladmin"
+}
+
+variable "engine_version" {
+  type    = string
+  default = "12"
+}
+
+
+variable "trusted_network_cidr" {
+  type = map(string)
+  default = {
+    azure_internal = "0.0.0.0/32"
+  }
+
+  description = <<-EOT
+  Trusted networks to allow access to e.g. Redis and PostgreSQL.
+  The key may only contain alphanumeric characters and underscores.
+  EOT
+}
+
+
+variable "tags" {
+  type        = map(string)
+  description = "The tags set allow to identify the resources that are managed by Terraform."
+  default = {
+    TF-Managed  = "true"
+    Maintainer  = ""
+    TF-Worfklow = ""
+  }
+}
+
+variable "configuration" {
+  type    = map(string)
+  default = {}
+}
